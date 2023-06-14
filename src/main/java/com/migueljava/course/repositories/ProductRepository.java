d3fa1bbb34df879518308ca4780ab692166eecc3
oxtw <weigertmiguel@hotmail.com>
2023-06-14 18:01:34 -0300

    Product

---
+++ src/main/java/com/migueljava/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.migueljava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.migueljava.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
