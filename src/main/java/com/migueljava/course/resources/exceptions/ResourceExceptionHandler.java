23c3a00c775a818c0dc281ab840813104dc4f4e5
oxtw <weigertmiguel@hotmail.com>
2023-07-10 15:59:02 -0300

    Exception handling - delete

--- src/main/java/com/migueljava/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/migueljava/course/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.migueljava.course.services.exceptions.DatabaseException;
 import com.migueljava.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,10 +16,19 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice //esta annotation que irá interceptar as exceções para que esse objeto possa exectar o tratamento.
 public class ResourceExceptionHandler {
 	
+	//Método para o ResourceNotFoundException.
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e , HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	//método para o DatabaseException
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> resourceNotFound(DatabaseException e , HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/migueljava/course/services/UserService.java
+++ src/main/java/com/migueljava/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.migueljava.course.entities.User;
 import com.migueljava.course.repositories.UserRepository;
+import com.migueljava.course.services.exceptions.DatabaseException;
 import com.migueljava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,8 +32,15 @@ public class UserService {
 		return repository.save(obj);
 	}
 	//metodo DELETE para excluir um user do banco de dados por ID.
+	//Utilizando metodos para tratamento de exceção.
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	//função para atualizar um dado UPDATE
 	public User update(Long id, User obj) {
